1208a71fdbc50f778de1e514fbc23ef5f3e00878
Uroko Emmanuel <euroko@Urokos-MacBook-Pro.local>
2025-02-17 21:27:49 +0100

    object.js and operanda.js were added

---
+++ object.js
@@ -0,0 +1,12 @@
+// Object can store multiple data in key value pairs
+const student = {
+  firstName: "Emmanuel",
+  color: "red",
+};
+
+console.log(student);
+
+//acessing he values in an object.
+//using the dot notation.
+console.log(student.firstName);
+console.log(student.color);
